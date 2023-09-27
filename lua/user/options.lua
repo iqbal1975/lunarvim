ace2cadc36e041464cace00fcd1b58f9aa7259ea
iqbal1975 <iqbal.thaker@gmail.com>
2023-09-27 12:25:05 +0530

    Update LunarVim configuration

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,38 +1,46 @@
--- vim options
-local options = {
-	colorcolumn = "90",
-	completeopt = { "menuone", "noinsert", "noselect" },
-	linebreak = true,
-	listchars = {
-		-- list of hidden characters
-		tab = "» ",
-		trail = "•",
-		precedes = "←",
-		extends = "→",
-		eol = "↲",
-		space = "␣",
-	},
-	nrformats = { "alpha", "octal", "hex" },
-	sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions",
-	shell = "/bin/zsh",
-	showbreak = "↪",
-	splitbelow = true,
-	splitright = true,
-	shiftwidth = 2,
-	tabstop = 2,
-	relativenumber = true,
-	wrap = false,
-	-- winbar = "%=%m %f"
-}
+-- Vim Options
+-- Global
+vim.g.mapleader = " " -- sets vim.g.mapleader
+vim.g.autoformat_enabled = true -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+vim.g.cmp_enabled = true -- enable completion at start
+vim.g.autopairs_enabled = true -- enable autopairs at start
+vim.g.diagnostics_mode = 3 -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+vim.g.icons_enabled = true -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+vim.g.ui_notifications_enabled = true -- disable notifications when toggling UI elements
 
-for k, v in pairs(options) do
-	vim.opt[k] = v
-end
+-- Optional
+vim.opt.colorcolumn = "90"
+vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
+vim.opt.linebreak = true
+vim.opt.listchars = {
+	-- list of hidden characters
+	tab = "» ",
+	trail = "•",
+	precedes = "←",
+	extends = "→",
+	eol = "↲",
+	space = "␣",
+}
+vim.opt.nrformats = { "alpha", "octal", "hex" }
+vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.opt.shell = "/bin/zsh"
+vim.opt.showbreak = "↪"
+vim.opt.shiftwidth = 2
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+-- vim.opt.number = true -- sets vim.opt.number
+-- vim.opt.relativenumber = true -- sets vim.opt.relativenumber
+vim.opt.signcolumn = "auto" -- sets vim.opt.signcolumn to auto
+vim.opt.spell = false -- sets vim.opt.spell
+-- vim.opt.statuscolumn = "%l %r"
+vim.opt.tabstop = 2
+-- vim.opt.winbar = "%=%m %f"
+vim.opt.wrap = false -- sets vim.opt.wrap
 
 vim.cmd([[filetype plugin indent on]])
 vim.diagnostic.config({ virtual_text = true })
 
--- general
+-- General
 -- lvim.log.level = "info"
 lvim.log.level = "warn"
 lvim.format_on_save = {
